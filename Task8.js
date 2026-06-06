3248d31e0c720723b9c91220c9c4fa3505a65b91
Sameer jha <jhasamirjha262@gmail.com>
2026-06-06 08:53:44 +0545

    vs code

---
+++ Task8.js
@@ -0,0 +1,12 @@
+//Rest Operator (Sum of Numbers)
+function sumAll(...numbers) {
+    let sum = 0;
+
+    for (let num of numbers) {
+        sum += num;
+    }
+
+    return sum;
+}
+
+console.log(sumAll(1, 2, 3, 4));
