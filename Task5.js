49d4e8e1f0afbed8b11f916dce50c13e55d877f5
Sameer jha <jhasamirjha262@gmail.com>
2026-06-06 08:51:30 +0545

    vs code

---
+++ Task5.js
@@ -0,0 +1,11 @@
+//Object Destructuring
+const person = {
+    name: "Sameer",
+    age: 20,
+    country: "Nepal"
+};
+
+const { name, country } = person;
+
+console.log(name);
+console.log(country);
