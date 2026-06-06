5b8b9ef0e9b1b362681dd2474fa86d050dc516e5
Sameer jha <jhasamirjha262@gmail.com>
2026-06-06 08:48:01 +0545

    vs code

--- README.md
+++
@@ -1,2 +0,0 @@
-# Day 1: Core JavaScript Hands-On Tasks
-
---
+++ Task1.js
@@ -0,0 +1,7 @@
+//Variables and Template Literals
+let firstName = "Sameer";
+let lastName = "Jha";
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+
+console.log(greeting);
